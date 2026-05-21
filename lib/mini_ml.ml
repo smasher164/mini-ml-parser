79c6a56f85cf25a86450976bd74e68f4d660b5c5
Akhil Indurti <aindurti@gmail.com>
2026-05-21 08:59:08 -0700

    document parse_string entry point

--- lib/mini_ml.ml
+++ lib/mini_ml.ml
@@ -2,6 +2,15 @@ module Ast = Ast
 module Lexer = Lexer
 module Parser = Parser
 
+(** Parse a complete program from [input] and return its AST: a tuple
+    of type declarations and an expression AST (see {!Ast.prog}).
+
+    The re2ocaml-generated lexer threads its own buffer through
+    [Lexer.lex], so the [Lexing.lexbuf] passed to Menhir is a dummy
+    that the lexer function ignores.
+
+    Raises [Failure] on an unexpected character and
+    [Parser.Error] on a syntax error. *)
 let parse_string (input : string) : Ast.prog =
   let buf = Lexer.make input in
   Parser.program (fun _ -> Lexer.lex buf) (Lexing.from_string "")
